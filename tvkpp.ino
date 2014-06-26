29995efc04dca9217a1a49d1e5d06e5a9317d22e
Matthew Nielsen <xunker@pyxidis.org>
2014-06-26 11:22:44 -0600

    renamed files, added readme

--- tvkpp.ino
+++ tvkpp.ino
@@ -1,4 +1,35 @@
-// see tutorial at http://learn.adafruit.com/trinket-usb-volume-knob
+/*
+  This is a modification of the Adafruit Trinket Volume Knob tutorial, changed
+  so the knob can do more than just change the volume. It uses the same wiring
+  as the TrinketVolumeKnobPlus example.
+
+    IMPORTANT NOTE:
+
+    As of June 26 2014, `TrinketHidCombo.h` from Adafruit does not include
+    support for mouse wheel events. I have forked the code and added support:
+    https://github.com/xunker/Adafruit-Trinket-USB
+
+    Hopefully my pull request will be accepted soon and the changes will merge
+    in to the Adafruit master. :)
+
+  This version allows you to change the commands being sent by "long pressing"
+  the knob for 1 second or more. A press of less than one second will execute
+  the normal button-press command.
+
+  To change the commands, edit `struct control_set control_sets[] = { ... }`.
+
+  By default, the command sets are:
+
+  | ROTATE LEFT           | ROTATE RIGHT          | PRESS BUTTON             |
+  |-----------------------|-----------------------|--------------------------|
+  | mouse wheel scroll up | mouse wheel scroll dn | home key (scroll to top) |
+  | volume down key       | volume down key       | mute key                 |
+  | previous track key    | next track key        | play/pause key           |
+  | page up key           | page down key         | home key                 |
+  | up arrow key          | down arrow key        | home key                 |
+
+  Original tutorial found at http://learn.adafruit.com/trinket-usb-volume-knob
+*/
 
 #include "TrinketHidCombo.h"
 
